a0fa275b1ca65fc6ec94453c40633f1b1bb2c041
Ryan Brandt <ryan.brandt@biomeme.com>
2020-05-13 00:02:51 -0400

    fix ci

--- lib/Elements/Button/Button.d.ts
+++ lib/Elements/Button/Button.d.ts
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as t from "./types";
 import "./index.css";
+import "../../global.css";
 declare const Button: React.FunctionComponent<t.Props>;
 export default Button;

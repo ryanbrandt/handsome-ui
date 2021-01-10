2953bbaa2156e1f922804cf038e28db50f605407
ryan.brandt@biomeme.com <rnb52@scarletmail.rutgers.edu>
2021-01-09 23:49:53 -0500

    custom file selector
    export new file selector

---
+++ lib/Inputs/FileInput/FileInput.d.ts
@@ -0,0 +1,22 @@
+import * as React from "react";
+import "./index.css";
+interface Props {
+    /**
+     * Function to execute on file selection change
+     */
+    onChange: (files: Array<File>) => void;
+    /**
+     * Optional label for file selector
+     */
+    label?: string;
+    /**
+     * Flag indicating whether multiple files may be selected
+     */
+    multiple?: boolean;
+    /**
+     * Optional string defining accepted file type (e.g. image/png)
+     */
+    accept?: string;
+}
+declare const FileInput: React.FunctionComponent<Props>;
+export default FileInput;
---
+++ src/Inputs/FileInput/FileInput.md
@@ -0,0 +1,7 @@
+Simple, stylized file input
+
+```tsx
+import FileInput from "./FileInput";
+
+<FileInput onChange={() => null} />;
+```

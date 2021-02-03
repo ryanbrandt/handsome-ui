90a0092d566a061203e4756fdca6b31269c96f9d
ryan.brandt@biomeme.com <rnb52@scarletmail.rutgers.edu>
2021-02-03 01:04:10 -0500

    prettier spinner

--- src/Elements/Spinner/Spinner.tsx
+++ src/Elements/Spinner/Spinner.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { combineClassNames } from "../../utils/helpers";
 
 import "./index.css";
 
@@ -14,7 +15,24 @@ const Spinner: React.FunctionComponent<Props> = (
 ): React.ReactElement => {
   const { className } = props;
 
-  return <div className={`${className ? `${className} ` : ""}spinner`} />;
+  const containerClassName = combineClassNames("spinner-circle", className);
+
+  return (
+    <div className={containerClassName}>
+      <div className="spinner-circle1 spinner-child"></div>
+      <div className="spinner-circle2 spinner-child"></div>
+      <div className="spinner-circle3 spinner-child"></div>
+      <div className="spinner-circle4 spinner-child"></div>
+      <div className="spinner-circle5 spinner-child"></div>
+      <div className="spinner-circle6 spinner-child"></div>
+      <div className="spinner-circle7 spinner-child"></div>
+      <div className="spinner-circle8 spinner-child"></div>
+      <div className="spinner-circle9 spinner-child"></div>
+      <div className="spinner-circle10 spinner-child"></div>
+      <div className="spinner-circle11 spinner-child"></div>
+      <div className="spinner-circle12 spinner-child"></div>
+    </div>
+  );
 };
 
 export default Spinner;
